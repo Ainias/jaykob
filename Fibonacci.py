36de66160d02c71393fd4d839869db5425e3f863
Jakob <Jakobprogrammiert74@gmail.com>
2022-12-07 08:32:38 +0100

    Rekursives Vorgehen GGT Dictionaries

--- Fibonacci.py
+++ Fibonacci.py
@@ -13,11 +13,22 @@ for i in range(1, n, 1):  # n wird nicht nochmal durchgegangen (exklusiv)
 
     # für i=0 f(i)=0/i=1 f(i)= 1, i>=2 f(i)=f(i-1)+f(i-2)
 
+result = {
+    0: 0,
+    1: 1
+}
+
 
 def fibo(zahl: int):
-    if zahl == 0 or zahl == 1:  # immer komplette Bedingung hinschreiben - also hier 2* 'zahl =='
-        return zahl
-    return fibo(zahl - 1) + fibo(zahl - 2)
+    if zahl not in result:  # Überprüfung ob key im Dictionary ist
+        # return result[zahl]  # mit if zahl in result muss das hier noch stehen --> hole dir die Zahl direkt aus dem Dictionary
+        #  if zahl == 0 or zahl == 1:  # immer komplette Bedingung hinschreiben - also hier 2* 'zahl =='
+        # return zahl
+        result[zahl] = fibo(zahl - 1) + fibo(zahl - 2)
+    return result[zahl]
 
 
-print(fibo(6))
+print(fibo(112))
+
+# Dictionaries für Laufzeitverkürzung
+# x = {"key" : "value"} bzw. Befehl für neue Einträge: x["newKey"] = "value"
--- GGT.py
+++ GGT.py
@@ -7,4 +7,16 @@ while rest > 0:
     zahl1 = zahl2
     zahl2 = rest
 print(zahl1)
+
+
 # nächstes Mal: Funktionen + Eingabeüberprüfung
+
+def ggt(zahl1: int, zahl2: int):
+    #  return ggt(zahl2, zahl1 % zahl2) if (zahl2 > 0) else zahl1
+    if zahl2 > 0:
+        return ggt(zahl2, zahl1 % zahl2)
+    return zahl1
+
+
+# kurze if Schreibweise: "a" if "Bedingung" else "b"
+print(ggt(5, 2))
